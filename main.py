b18e65589bb3432339bb314f862e5099fe5355ee
bboyriki <100321831+bboyriki@users.noreply.github.com>
2022-10-11 17:21:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player_1 = 'Guilt'
+goal_0 = 32
+player_2 = 'Van Basten'
+goal_1= 54
+report = player_1 +" " + "scored in the" + " " + " " + str(goal_0) + " " + "and" + " " + player_2 + " scored in the" + " " + str(goal_1)
+print (report)
+player = " frank rijkaard"
+first_name = player[1:6]
+print(first_name)
+last_name_len = len(player[7:15])
+print(last_name_len)
+name_short = slice (player) 
+print(name_short)
+chant = first_name + "!" + " " + first_name + "!"  + " " + first_name + "!" + " " + first_name + "!" + " " +first_name + "!"
+print(chant)
+good_chant = chant[-1] != "!"
+print (good_chant)
